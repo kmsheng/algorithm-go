1fd1f2aac6e82f25d03119b88dc5f49b1648b620
kmsheng <kmsh3ng@gmail.com>
2021-09-29 16:09:03 +0800

    fix typo: TestIsUniq -> TestIsPermutation

--- question/is_permutation_test.go
+++ question/is_permutation_test.go
@@ -6,7 +6,7 @@ import (
 	q "github.com/kmsheng/algorithm-go/question"
 )
 
-func TestIsUniq(t *testing.T) {
+func TestIsPermutation(t *testing.T) {
 	assert.True(t, q.IsPermutation("abc", "bca"))
 	assert.False(t, q.IsPermutation("abd", "bca"))
 }
